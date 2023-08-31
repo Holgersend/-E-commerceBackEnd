63166a52a5e1ecdac0d40b3c9b8a44addbed4801
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-08-31 01:38:13 -0500

    Update category-routes.js
    Created a rout to update a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,9 +40,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.put('/:id', (req, res) => {
-//   // update a category by its `id` value
-// });
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const updateCategory= await Category.update(req.body, {where: { id: req.params.id}});
+  } catch (err) {
+    res.status(500).json({message: `Failed to update!`});
+  }
+});
 
 // router.delete('/:id', (req, res) => {
 //   // delete a category by its `id` value
