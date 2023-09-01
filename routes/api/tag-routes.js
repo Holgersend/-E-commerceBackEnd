f56ab030ef6bb9f0e11e40c07ff741d92329c70a
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-08-31 20:57:23 -0500

    Update tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,9 +34,9 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const newTag = await newTag.create({
-      reader_id: req.body.reader_id,
-    });
+    const newTag = await newTag.create(
+      req.body
+    );
     res.status(200).json(newTag);
   } catch (err) {
     res.status(400).json(err);
